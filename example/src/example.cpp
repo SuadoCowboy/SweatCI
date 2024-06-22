0c41b5b5c7eb95d335c436f63bc310c45faacf6e
Suado Cowboy <luccacsg@gmail.com>
2024-06-22 00:32:45 -0300

    removed CVariable that was only being used to test void* because of lifetime, vector resizing and memory shit

--- example/src/example.cpp
+++ example/src/example.cpp
@@ -43,11 +43,6 @@ int main()
         [&](const std::string& value){userName = value;},
         [&](){return userName;},
         "- the name of the user :P");
-    
-    HayBCMD::CVARStorage::setCvar("test",
-        [&](const std::string&){},
-        [&](){return "never!";},
-        "- literally shit");
 
     HayBCMD::Command("+test", 0, 0, [](void*, const std::vector<std::string>&){
         HayBCMD::Output::print(HayBCMD::ECHO, ":)\n");
