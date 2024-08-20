630a199d457d6d55a156873a071c042dfc21b32a
Suado Cowboy <luccacsg@gmail.com>
2024-08-20 12:40:08 -0300

    created CVARUtils::setBoolean and getBoolean and removed unsigned char stuff because it would cost more space

--- HayBCMD.cpp
+++ HayBCMD.cpp
@@ -379,17 +379,15 @@ namespace HayBCMD {
         return *(std::string*)pData;
     }
 
-    void CVARUtils::setUnsignedCharBitwiseFlag(void *pData, const std::string& value) {
+    void CVARUtils::setBoolean(void *pData, const std::string& value) {
         try {
-            auto data = *(UnsignedCharBitwiseFlag*)pData;
-            if (std::stoi(value) <= 0) data.flags &= ~(data.bit);
-            else data.flags |= data.bit;
+            if (std::stoi(value) <= 0) *(bool*)pData = false;
+            else *(bool*)pData = true;
         } catch (...) {return;}
     }
 
-    std::string CVARUtils::getUnsignedCharBitwiseFlag(void *pData) {
-        auto data = *(UnsignedCharBitwiseFlag*)pData;
-        return std::to_string((data.flags & data.bit) == data.bit);
+    std::string CVARUtils::getBoolean(void *pData) {
+        return std::to_string(*(bool*)pData);
     }
 
     void CVARStorage::setCvar(const std::string& name, void* pData, void(*set)(void *pData, const std::string &value), std::string (*toString)(void *pData), const std::string& usage) {
--- HayBCMD.h
+++ HayBCMD.h
@@ -179,18 +179,11 @@ namespace HayBCMD {
     };
 
     namespace CVARUtils {
-        struct UnsignedCharBitwiseFlag {
-            unsigned char& flags;
-            unsigned char bit;
-
-            UnsignedCharBitwiseFlag(unsigned char& flags, unsigned char bit) : flags(flags), bit(bit) {}
-        };
-
         void setString(void *pData, const std::string& value);
         std::string getString(void *pData);
 
-        void setUnsignedCharBitwiseFlag(void *pData, const std::string& value);
-        std::string getUnsignedCharBitwiseFlag(void *pData);
+        void setBoolean(void *pData, const std::string& value);
+        std::string getBoolean(void *pData);
     }
 
     struct CVariable {
