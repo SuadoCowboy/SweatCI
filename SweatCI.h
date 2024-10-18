b289f1f69a6d13aed8549e9af68d14deb02d3709
SuadoCowboy <luccacsg@gmail.com>
2024-10-18 11:50:46 -0300

    numbers with value 0 will now appear 0 instead of blank

--- SweatCI.h
+++ SweatCI.h
@@ -182,9 +182,10 @@ namespace SweatCI {
     template<typename T>
     std::string numberToString(T value) {
         std::string str = std::to_string(value);
-        str.erase(str.find_last_not_of('0') + 1, std::string::npos);
-        str.erase(str.find_last_not_of('.') + 1, std::string::npos);
-
+	if (str != "0") {
+        	str.erase(str.find_last_not_of('0') + 1, std::string::npos);
+        	str.erase(str.find_last_not_of('.') + 1, std::string::npos);
+	}
         return str;
     }
 
